43ad00280d4b3cf911608f5782db38108e0387aa
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-07-29 11:52:17 +0200

    Add color gradient on outside

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,34 @@
 #include <raylib.h>
 #include <vector>
 
-bool in_mandelbrot_set(std::complex<double> c) {
+Color gradient(Color start, Color end, double point) {
+    auto scale = [](auto s, auto e, auto p) { return s * (1.0 - p) + e * p; };
+    return Color{
+        (unsigned char)scale(start.r, end.r, point),
+        (unsigned char)scale(start.g, end.g, point),
+        (unsigned char)scale(start.b, end.b, point),
+        (unsigned char)scale(start.a, end.a, point),
+    };
+}
+
+Color in_mandelbrot_set(std::complex<double> c) {
     constexpr int max_iters = 200;
     std::complex<double> z{0.0, 0.0};
     for (int i = 0; i < max_iters; i++) {
         if (std::abs(z) > 2) {
-            return false;
+            Color start = {0x44, 0x01, 0x54, 0xff};
+            Color end = {0xfd, 0xe7, 0x25, 0xff};
+            return gradient(start, end, (double)i / max_iters);
         }
         z = z * z + c;
     }
-    return true;
+    return Color{0x00, 0x00, 0x00, 0xff};
 }
 
 int main() {
     constexpr int width = 400;
     constexpr int height = 400;
-    std::vector<bool> points(width * height);
+    std::vector<Color> points(width * height);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             double x = (double)j / width * (2.0 + 0.47) - 2.0;
@@ -34,9 +46,7 @@ int main() {
         ClearBackground(WHITE);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (points[width * i + j]) {
-                    DrawPixel(j, i, BLACK);
-                }
+                DrawPixel(j, i, points[width * i + j]);
             }
         }
 
